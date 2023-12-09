e68533939a0b9a3a4cce05bbe2f560befe3a8b18
CarltonNcube <carltonncube@outlook.com>
2023-12-09 12:29:29 -0800

    a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 import os.path
 from fabric.api import env, put, run
 
-env.hosts = ["54.173.251.99", "52.91.125.177"]
+env.hosts = ["54.173.251.99", "52.91.125.177"]  # Update with your IP addresses
 
 
 def do_deploy(archive_path):
@@ -15,17 +15,19 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if not os.path.exists(archive_path):
+    if not os.path.isfile(archive_path):
         return False
 
-    file = os.path.basename(archive_path)
-    name = os.path.splitext(file)[0]
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
     # Upload the archive to /tmp/ directory of the web server
     if put(archive_path, f"/tmp/{file}").failed:
         return False
 
     # Uncompress the archive to /data/web_static/releases/<archive filename without extension>
+    if run(f"rm -rf /data/web_static/releases/{name}/").failed:
+        return False
     if run(f"mkdir -p /data/web_static/releases/{name}/").failed:
         return False
     if run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}/").failed:
@@ -35,13 +37,22 @@ def do_deploy(archive_path):
     if run(f"rm /tmp/{file}").failed:
         return False
 
-    # Delete the symbolic link /data/web_static/current from the web server
+    # Move contents to the release directory
+    if run(f"mv /data/web_static/releases/{name}/web_static/* "
+           f"/data/web_static/releases/{name}/").failed:
+        return False
+
+    # Delete unnecessary web_static directory
+    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed:
+        return False
+
+    # Delete the current symbolic link
     if run("rm -rf /data/web_static/current").failed:
         return False
 
-    # Create a new symbolic link /data/web_static/current on the web server
-    if run(f"ln -s /data/web_static/releases/{name}/ /data/web_static/current").failed:
+    # Create a new symbolic link
+    if run(f"ln -s /data/web_static/releases/{name}/ "
+           f"/data/web_static/current").failed:
         return False
 
     return True
-
