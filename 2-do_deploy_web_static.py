abc4905b37138cd5bba58985e838078c10fa41ad
CarltonNcube <carltonncube@outlook.com>
2023-12-09 11:53:31 -0800

    a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 import os.path
 from fabric.api import env, put, run
 
-env.hosts = ["54.173.251.99", "52.91.125.177"]  # Update with your IP addresses
+env.hosts = ["54.173.251.99", "52.91.125.177"]
 
 
 def do_deploy(archive_path):
@@ -21,25 +21,27 @@ def do_deploy(archive_path):
     file = os.path.basename(archive_path)
     name = os.path.splitext(file)[0]
 
+    # Upload the archive to /tmp/ directory of the web server
     if put(archive_path, f"/tmp/{file}").failed:
         return False
-    if run(f"rm -rf /data/web_static/releases/{name}/").failed:
-        return False
+
+    # Uncompress the archive to /data/web_static/releases/<archive filename without extension>
     if run(f"mkdir -p /data/web_static/releases/{name}/").failed:
         return False
     if run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}/").failed:
         return False
+
+    # Delete the archive from the web server
     if run(f"rm /tmp/{file}").failed:
         return False
-    if run(f"mv /data/web_static/releases/{name}/web_static/* "
-           f"/data/web_static/releases/{name}/").failed:
-        return False
-    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed:
-        return False
+
+    # Delete the symbolic link /data/web_static/current from the web server
     if run("rm -rf /data/web_static/current").failed:
         return False
-    if run(f"ln -s /data/web_static/releases/{name}/ "
-           f"/data/web_static/current").failed:
+
+    # Create a new symbolic link /data/web_static/current on the web server
+    if run(f"ln -s /data/web_static/releases/{name}/ /data/web_static/current").failed:
         return False
 
     return True
+
