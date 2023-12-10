f88acfb7ed297d651793311b9e8df6f63d00ed23
CarltonNcube <carltonncube@outlook.com>
2023-12-10 08:56:39 -0800

    a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Fabfile to create and distribute an archive to web servers.
-from fabric.api import env, local, run
+from fabric.api import env, local, run, put
 from datetime import datetime
 from os.path import isfile
 from pathlib import Path
@@ -11,14 +11,18 @@ env.key_filename = "~/.ssh/0-use_a_private_key"
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
-    )
-    if not Path("versions").is_dir():
-        local("mkdir -p versions")
-    result = local("tar -cvzf {} web_static".format(file))
-    return file if result.succeeded else None
+    try:
+        dt = datetime.utcnow()
+        file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+        )
+        if not Path("versions").is_dir():
+            local("mkdir -p versions")
+        result = local("tar -cvzf {} web_static".format(file))
+        return file if result.succeeded else None
+    except Exception as e:
+        print(e)
+        return None
 
 def do_deploy(archive_path):
     """Distribute an archive to web servers.
@@ -39,8 +43,30 @@ def do_deploy(archive_path):
     remote_release = "/data/web_static/releases/{}/".format(name)
 
     try:
-        local("fab -f 2-do_deploy_web_static.py do_deploy:archive_path={}"
-              .format(archive_path))
+        # Upload the archive to /tmp/ directory of the web server
+        put(archive_path, remote_tmp)
+
+        # Create a new release directory
+        run('mkdir -p {}'.format(remote_release))
+
+        # Extract the archive to the new release directory
+        run('tar -xzf {} -C {}'.format(remote_tmp, remote_release))
+
+        # Remove the uploaded archive from /tmp/
+        run('rm {}'.format(remote_tmp))
+
+        # Move contents to the release directory
+        run('mv {}web_static/* {}'.format(remote_release, remote_release))
+
+        # Remove unnecessary web_static directory
+        run('rm -rf {}web_static'.format(remote_release))
+
+        # Remove the current symbolic link
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link
+        run('ln -s {} /data/web_static/current'.format(remote_release))
+
         return True
     except Exception as e:
         print(e)
