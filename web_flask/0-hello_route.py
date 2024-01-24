e93d8c4eef93378dd71c168126aa2308346afa90
CarltonNcube <carltonncube@outlook.com>
2024-01-24 11:27:16 -0800

    Set up development with Python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application.
+Start a Flask web application.
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-    """Route that displays 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
